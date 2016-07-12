38cf6aec5db68c946ab7ba2502df98c3264754fb
qsctr <brettonchen@gmail.com>
2016-07-12 11:13:34 +0800

    Change enter key behavior

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,11 +24,24 @@ public class MainActivity extends AppCompatActivity {
         button = (Button)findViewById(R.id.button);
         editText = (EditText)findViewById(R.id.editText);
         textView.setText(R.string.app_name);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER
+                && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    submit(v);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void submit(View view) {
         if (!submitted) {
             textView.setText("You submitted: " + editText.getText());
+            editText.setText("");
             button.setText(R.string.button_unsubmit);
             submitted = true;
         } else {
