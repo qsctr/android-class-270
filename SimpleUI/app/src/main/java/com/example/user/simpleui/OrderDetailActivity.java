c01de783cec44ddc31f08eef7882e704ef273a3a
qsctr <brettonchen@gmail.com>
2016-07-22 10:58:15 +0800

    Find and show current location on map

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,15 +1,25 @@
 package com.example.user.simpleui;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,12 +31,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.lang.ref.WeakReference;
 import java.util.List;
-import java.util.Stack;
 
-public class OrderDetailActivity extends AppCompatActivity implements GeoCodingTask.GeoCodingResponse {
+public class OrderDetailActivity
+        extends AppCompatActivity
+        implements GeoCodingTask.GeoCodingResponse,
+            GoogleApiClient.ConnectionCallbacks,
+            GoogleApiClient.OnConnectionFailedListener,
+            LocationListener {
 
-    GoogleMap googleMap;
+    final static int ACCESS_FINE_LOCATION_REQUEST_CODE = 1;
+
     String storeInfo;
+    GoogleMap googleMap;
+    GoogleApiClient googleApiClient;
+    LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,19 +88,93 @@ public class OrderDetailActivity extends AppCompatActivity implements GeoCodingT
     @Override
     public void responseWithGeoCodingResults(LatLng latlng) {
         if (googleMap != null) {
-            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 6);
-            MarkerOptions markerOptions = new MarkerOptions().position(latlng).title(storeInfo);
-            googleMap.animateCamera(cameraUpdate);
-            googleMap.addMarker(markerOptions);
+            // CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 6);
+            // MarkerOptions markerOptions = new MarkerOptions().position(latlng).title(storeInfo);
+            // googleMap.animateCamera(cameraUpdate);
+            // googleMap.addMarker(markerOptions);
 
-            googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                @Override
-                public boolean onMarkerClick(Marker marker) {
-                    Log.d("Debug", "Marker clicked");
-                    return false;
-                }
-            });
+//            googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+//                @Override
+//                public boolean onMarkerClick(Marker marker) {
+//                    Log.d("Debug", "Marker clicked");
+//                    return false;
+//                }
+//            });
+
+            createGoogleApiClient();
         }
+    }
+
+    private void createGoogleApiClient() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d("Debug", "Not granted");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                        ACCESS_FINE_LOCATION_REQUEST_CODE);
+            }
+            return;
+        }
+
+        createLocationRequest();
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        LatLng start = new LatLng(25.0186348, 121.5398379);
+        if (location != null) {
+            start = new LatLng(location.getLatitude(), location.getLongitude());
+        }
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(start, 6));
+        Log.d("Debug", "Location found");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == ACCESS_FINE_LOCATION_REQUEST_CODE) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION)
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onConnected(null);
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    private void createLocationRequest() {
+        if (locationRequest == null) {
+            locationRequest = new LocationRequest()
+                    .setInterval(1000)
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            LocationServices.FusedLocationApi
+                    .requestLocationUpdates(googleApiClient, locationRequest, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 6));
     }
 
 //    public static class GeoCodingTask extends AsyncTask<String, Void, Bitmap> {
